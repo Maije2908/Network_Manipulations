6ddc6a112a87e7d634a6ffcaf7339b3396ef1e14
Maije <christoph.m@live.at>
2024-12-05 15:48:13 +0100

    Update SParam_Calculations.py
    Added the differential S-Parameter class.

--- SParam_Calculations.py
+++ SParam_Calculations.py
@@ -1,35 +1,88 @@
 # -*- coding: utf-8 -*-
 """
 last change: 05.12.2024
-@author: christoph_m
+@author: Christoph Maier
 
-This function calculates the NMSE of two S-parameter objects by comparing
-the transmission and the reflection coefficients separately.
-
-  Input Parameters:
-      SComp  ... network object of the S-parameter block which is
-                 compared to the reference one
-      SRef   ... network object used as reference. The frequency grid
-                 and the number of ports of the two S-parameter objects
-                 must be the same
-                 If this variable is left empty, a comparison to a
-                 infinitesimally small, perfecly matched line
-      ShowdB ... Show the calculated NMSE in dB (instead of its linear
-                 value)
-      ShowCMD ... Write the results to the command window
-
-  Output parameters:
-      NMSERef... Calculated NMSE for the reflection coefficients 
-      NMSETrans... Calculated NMSE for the transmission coefficients
-
-  needed packages:
-      numpy
 
 """
 
 # import needed packages
 import numpy as np 
 import skrf as rf
+
+
+"""
+    A class to represent mixed-mode S-parameters (scattering parameters) for 
+    a network over a range of frequencies.
+
+    Attributes:
+        frequency (list): A list of the frequency points.
+        
+        Differential-to-Differential S-parameters:
+        - Sdd11: Input reflection coefficient for differential signals.
+        - Sdd12: Reverse transmission coefficient for differential signals.
+        - Sdd21: Forward transmission coefficient for differential signals.
+        - Sdd22: Output reflection coefficient for differential signals.
+
+        Differential-to-Common S-parameters:
+        - Sdc11: Reflection coefficient from differential to common-mode at input.
+        - Sdc12: Reverse transmission coefficient from differential to common-mode.
+        - Sdc21: Forward transmission coefficient from differential to common-mode.
+        - Sdc22: Reflection coefficient from differential to common-mode at output.
+
+        Common-to-Differential S-parameters:
+        - Scd11: Reflection coefficient from common-mode to differential at input.
+        - Scd12: Reverse transmission coefficient from common-mode to differential.
+        - Scd21: Forward transmission coefficient from common-mode to differential.
+        - Scd22: Reflection coefficient from common-mode to differential at output.
+
+        Common-to-Common S-parameters:
+        - Scc11: Input reflection coefficient for common-mode signals.
+        - Scc12: Reverse transmission coefficient for common-mode signals.
+        - Scc21: Forward transmission coefficient for common-mode signals.
+        - Scc22: Output reflection coefficient for common-mode signals.
+
+    Methods:
+        None
+"""
+class MixedModeParameter:
+    def __init__(self, frequency, sdd11_re, sdd11_im, sdd12_re, sdd12_im,
+                 sdc11_re, sdc11_im, sdc12_re, sdc12_im, sdd21_re, sdd21_im,
+                 sdd22_re, sdd22_im, sdc21_re, sdc21_im, sdc22_re, sdc22_im,
+                 scd11_re, scd11_im, scd12_re, scd12_im, scc11_re, scc11_im,
+                 scc12_re, scc12_im, scd21_re, scd21_im, scd22_re, scd22_im,
+                 scc21_re, scc21_im, scc22_re, scc22_im):
+        
+        """
+        Initializes the MixedModeParameter class by combining the real and imaginary
+        parts of the S-parameters into lists of complex numbers for each parameter.
+    
+        Parameters:
+            frequency (list): A list of frequency points.
+            sdd11_re, sdd11_im, ..., scc22_im (list): Real and imaginary parts of the S-parameters.
+    
+        Each S-parameter is represented as a list of complex numbers corresponding
+        to each frequency point.
+        """
+        self.frequency = frequency
+        self.Sdd11 = [complex(r,i) for r, i in zip(sdd11_re, sdd11_im)]
+        self.Sdd12 = [complex(r,i) for r, i in zip(sdd12_re, sdd12_im)]
+        self.Sdc11 = [complex(r,i) for r, i in zip(sdc11_re, sdc11_im)]
+        self.Sdc12 = [complex(r,i) for r, i in zip(sdc12_re, sdc12_im)]
+        self.Sdd21 = [complex(r,i) for r, i in zip(sdd21_re, sdd21_im)]
+        self.Sdd22 = [complex(r,i) for r, i in zip(sdd22_re, sdd22_im)]
+        self.Sdc21 = [complex(r,i) for r, i in zip(sdc21_re, sdc21_im)]
+        self.Sdc22 = [complex(r,i) for r, i in zip(sdc22_re, sdc22_im)]
+        self.Scd11 = [complex(r,i) for r, i in zip(scd11_re, scd11_im)]
+        self.Scd12 = [complex(r,i) for r, i in zip(scd12_re, scd12_im)]
+        self.Scc11 = [complex(r,i) for r, i in zip(scc11_re, scc11_im)]
+        self.Scc12 = [complex(r,i) for r, i in zip(scc12_re, scc12_im)]
+        self.Scd21 = [complex(r,i) for r, i in zip(scd21_re, scd21_im)]
+        self.Scd22 = [complex(r,i) for r, i in zip(scd22_re, scd22_im)]
+        self.Scc21 = [complex(r,i) for r, i in zip(scc21_re, scc21_im)]
+        self.Scc22 = [complex(r,i) for r, i in zip(scc22_re, scc22_im)]
+        
+        
 
 # set default values of the variables
 eps = np.finfo(np.float64).eps
@@ -59,9 +112,27 @@ def set_showdB():
 def reset_showdB():
     global ShowdB
     ShowdB = False
+   
     
-
-
+   
+'''
+    This function calculates the normalized mean-square error (NMSE) of two
+    S-parameter objects by comparing the transmission and the reflection
+    coefficients separately.
+    
+    Input Parameters:
+        SComp  ... network object of the S-parameter block which is
+                   compared to the reference one
+        SRef   ... network object used as reference. The frequency grid
+                   and the number of ports of the two S-parameter objects
+                   must be the same
+                   If this variable is left empty, a comparison to a
+                   infinitesimally small, perfecly matched line
+    
+    Output parameters:
+        NMSERef... Calculated NMSE for the reflection coefficients 
+        NMSETrans... Calculated NMSE for the transmission coefficients
+'''
 def CalcSParameterNMSE(SComp, SRef):
      
     # generate variables
