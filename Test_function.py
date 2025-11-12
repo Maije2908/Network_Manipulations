9fc88d8563b1a1b7a45316d1710e054e5274352e
Maije <christoph.m@live.at>
2025-11-12 11:25:57 +0100

    cleared test function, added test for NMSE calculation

--- Test_function.py
+++ Test_function.py
@@ -6,30 +6,27 @@ Created on Thu Dec  5 10:56:46 2024
 """
 
 import skrf as rf
-import numpy as np
-import matplotlib.pyplot as plt
-
 import network_manipulations as netman
 
 if __name__ =='__main__':
         
+    # define test files
     ntwk_1 = rf.Network('exam_1.s4p') # 4-port, 4001 pnt
     ntwk_2 = rf.Network('exam_2.s4p') # 4-port, 4001 pnt
     ntwk_3 = rf.Network('exam_3.s4p') # 4-port, 3995 pnt
     ntwk_4 = rf.Network('exam_4.s2p') # 2-port, 4001 pnt
     ntwk_5 = rf.Network('exam_5.s4p') # empty
         
-    path = 'a1'
-    
-    ntwk = netman.ZNAExtraction(path)
-    
-    
-    ShowdB = 0
-        
+    # set or reset flags
     netman.set_showCMD() 
     netman.set_showdB()
-
-       
-    a, b = netman.CalcSParameterNMSE(ntwk_1, ntwk_2)
-
+    
+    netman.reset_showCMD()
+    netman.reset_showdB()
+    
+    ### Example useages ###
+    # calculate the normalized mean-square error of two network objects 
+    # Flags control if answer is in dB or not
+    NMSE_reflect, NMSE_transm = netman.CalcSParameterNMSE(ntwk_1, ntwk_2)
+ 
     
