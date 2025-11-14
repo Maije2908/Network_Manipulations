5f049e9525b8ef158ae687aaa7b73252264450ae
Maije <christoph.m@live.at>
2025-11-14 09:44:59 +0100

    Added sentence to description

--- network_manipulations.py
+++ network_manipulations.py
@@ -4,6 +4,7 @@ last change: 13.11.2025
 Author(s): Christoph Maier
 
 This module is a selection of functions for the manipulations of S-Parameters.
+This file consists only for overview reasons
 """
 
 # import my functions
