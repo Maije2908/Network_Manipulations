82e65bba40dc3f6d27693f86e26a0f23520e2b03
Maije <christoph.m@live.at>
2025-11-14 14:20:00 +0100

    Typo.

--- network_manipulations.py
+++ network_manipulations.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-last change: 13.11.2025
+last change: 14.11.2025
 Author(s): Christoph Maier
 
 This module is a selection of functions for the manipulations of S-Parameters.
-This file consists only for overview reasons
+This file consists only for overview reasons.
 """
 
 # import my functions
