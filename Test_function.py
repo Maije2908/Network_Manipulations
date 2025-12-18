06414a64e1650a114a3149a05fbb9fc60d63bfea
Maije <christoph.m@live.at>
2025-12-18 08:55:57 +0100

    Header added and made package out of it

--- Test_function.py
+++ Test_function.py
@@ -166,4 +166,3 @@ if __name__ =='__main__':
                           savename='impedance.png')
     
     
-    
---
+++ network_manipulations/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+last change: 18.12.2025
+Author(s): Christoph Maier
+
+network_manipulations
+=====================
+
+Used for different network manipulations.
+
+This package bundles together:
+- extracting S and MM parameters out of network objects
+- slice S-Parameters
+- calculate MM parameters out of S parameters
+- calulate the NMSE of two networks
+- calculate impedance out of S-parameters (one-port, series-thru, shunt-thru)
+- div. plotting functions
+
+Intended usage:
+    import network_manipulations as netman
+
+    netman.extract_Sparam(...) or netman.SParams.extract_Sparam(...)
+    netman.plot_impedance(...) or netman.plot_functions.plot_impedance(...)
+"""
+
+from .myclasses import *
+from .plot_functions import *
+from .SParams import *
