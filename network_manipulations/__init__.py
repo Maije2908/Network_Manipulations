ac40ef46c8c1ca60371b95b86ba9d94126c2eaf0
Maije <christoph.m@live.at>
2026-06-16 13:39:23 +0200

    added right calls for the functions and classes.

--- network_manipulations/__init__.py
+++ network_manipulations/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-last change: 18.12.2025
+last change: 16.06.2026
 Author(s): Christoph Maier
 
 network_manipulations
@@ -19,6 +19,33 @@ This package bundles together:
 Intended usage:
     import network_manipulations as netman
 
-    netman.extract_Sparam(...) or netman.SParams.extract_Sparam(...)
-    netman.plot_impedance(...) or netman.plot_functions.plot_impedance(...)
+    netman.extract_Sparam(...)
+    netman.plot_impedance(...)
 """
+
+from .myclasses import MixedModeParameter
+from .osci_scripts import read_csv_1trace, mul_measurements_1ch, time_normalizer, multiplot
+from .plot_functions import conv_plot_values, plot_values, plot_Sparam, plot_comp_Sparam, plot_impedance
+from .SParams import extract_Sparam, extract_MMparam, slice_Sparam, S_to_MM, calc_Sparam_NMSE, calc_imp_oneport, calc_imp_seriesthru, calc_imp_shuntthru
+
+# __all__ is optional
+# Define package’s public API and control what gets imported
+# when someone uses: from network_manipulations import *
+__all__ = ["MixedModeParameter",
+           "read_csv_1trace",
+           "mul_measurements_1ch",
+           "time_normalizer",
+           "multiplot",
+           "conv_plot_values",
+           "plot_values",
+           "plot_Sparam",
+           "plot_comp_Sparam",
+           "plot_impedance",
+           "extract_Sparam",
+           "extract_MMparam",
+           "slice_Sparam",
+           "S_to_MM",
+           "calc_Sparam_NMSE",
+           "calc_imp_oneport",
+           "calc_imp_seriesthru",
+           "calc_imp_shuntthru"]
